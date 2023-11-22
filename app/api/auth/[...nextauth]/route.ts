a4c64fd4ab2065fb7deda49be6d4beff587f28dc
Maschkov Konstantin <maschkovc@yandex.ru>
2023-11-22 16:19:03 +0300

    fx

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,6 @@
 
 import NextAuth from "next-auth/next";
 
-const handler = NextAuth({});
+// const handler = NextAuth({});
 
-export {handler as GET, handler as POST}
+// export {handler as GET, handler as POST}
